3c8dbc1466449f80af04a23842fd67832e42b015
Taufik Anwar <anwrtaufik@gmail.com>
2024-06-30 15:18:20 +0700

    fix: connection issues

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM node:18.16.0-alpine3.17
-RUN mkdir -p /opt/app/ws-server
-WORKDIR /opt/app/ws-server
+RUN mkdir -p /usr/src/app/ws-server
+WORKDIR /usr/src/app/ws-server
+
 COPY package*.json ./
 RUN npm install
 COPY src/ ./src
+
 EXPOSE 8081
 CMD ["npm", "run", "pm2"]
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080});
 
@@ -8,20 +8,31 @@ let backendClient = null;
 wss.on("connection", (ws, req) => {
   console.log("New connection");
   console.log(req.headers);
-  const headers = req.headers["sec-websocket-protocol"].split(",");
-  const clientType = headers[0];
-
+  const clientType = req.headers["sec-websocket-protocol"];
 
   if (clientType === "arduino") {
     const espId = req.headers["x-esp32-id"];
+    console.log(`ESP32 connected with id: ${espId}`);
     espClients.set(espId, ws);
 
-    ws.on("message", (message) => {
-      const data = JSON.parse(message);
+    console.log(`Number of ESP32 clients: ${espClients.size}`);
+    console.log(`Number of backend clients: ${backendClient ? 1 : 0}`);
 
+    ws.on("message", (message) => {
+      const data = message.toString("base64");
+      const res = JSON.stringify({
+        espId: espId,
+        data: data
+      });   
+
+      console.log(`backend ready state: ${backendClient.readyState}`);
       if (backendClient && backendClient.readyState === WebSocket.OPEN) {
-        backendClient.send(JSON.stringify({ espId, ...data }));
+        backendClient.send(res);
       }
+    });
+
+    ws.on("error", (error) => {
+      console.log(`Error: ${error}`);
     });
 
     ws.on("close", () => {
@@ -29,6 +40,8 @@ wss.on("connection", (ws, req) => {
     });
   } else if (clientType === "backend") {
     backendClient = ws;
+
+    console.log(`backend client ${backendClient}`);
 
     backendClient.on("message", (message) => {
       const data = JSON.parse(message);
@@ -39,10 +52,16 @@ wss.on("connection", (ws, req) => {
       if (esp32Id && espClients.has(esp32Id)) {
         const espClient = espClients.get(esp32Id);
 
+        console.log(`espClient ready state: ${espClient.readyState}`);
+
         if (espClient && espClient.readyState === WebSocket.OPEN) {
           espClient.send(JSON.stringify(data));
         }
       }
+    });
+
+    backendClient.on("error", (error) => {
+      console.log(`Error: ${error}`);
     });
 
     backendClient.on("close", () => {
